3ff49ac2b4f9133be26d928d68b0f47a357bdcdb
Your Name <you@example.com>
2023-03-02 23:08:59 +0600

    dependencies.

--- build/main.js
+++ build/main.js
@@ -1,5 +1,2 @@
-import express from "express";
-let app = express();
-app.listen(8771, () => {
-    console.log("Running stackoverflow clone on http://localhost:8871");
-});
+console.log("StackOverflowAPIClone");
+export {};
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,1 @@
-import express from "express";
-
-let app = express();
-
-app.listen(8771, () => {
-   console.log("Running stackoverflow clone on http://localhost:8871")
- })
+console.log("StackOverflowAPIClone")
