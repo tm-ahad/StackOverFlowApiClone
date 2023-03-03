934ce728ee59ace3e56b5f3a6e79be613fbde01d
Your Name <you@example.com>
2023-03-03 09:52:01 +0600

    mongoose added

--- build/main.js
+++ build/main.js
@@ -1,2 +1,4 @@
+import { connect } from "mongoose";
+connect("mongodb://127.0.0.1:27017/StackOverflowDB")
+    .then(() => console.log("Hello World!"));
 console.log("StackOverflowAPIClone");
-export {};
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,6 @@
+import { connect } from "mongoose"
+
+connect("mongodb://127.0.0.1:27017/StackOverflowDB")
+	.then(() => console.log("Hello World!"))
+
 console.log("StackOverflowAPIClone")
